3e44b6434702d5caaf8922de16d21d522468e84f
Megan Hessel <meganhessel@ucsb.edu>
2025-08-05 12:35:32 -0700

    Created Graphs

--- DerivativePP.R
+++ DerivativePP.R
@@ -42,3 +42,21 @@ y <- seq(from = -0.4, to = 2.0, by = 0.1)
 
 # evaluate the slope of T(y) at each of those values 
 eval(dy_dx)
+
+#one small change
+
+
+# plotting example 
+library(ggplot2)
+library(palmerpenguins)
+
+ggplot(data = penguins, aes(x = body_mass_g, y = flipper_length_mm)) +
+  geom_point(aes(color = species)) + 
+  scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+  labs(x = "Body Mass (g)", 
+       y = "Flipper Length (mm)", 
+       title = "Palmer Penguins Size Measurements", 
+       subtitle = "Palmer archipelago, Antarctica (2007 - 2009", 
+       caption = "Collected by Dr. Kristen Gorman") +
+  facet_wrap(~island) +
+  theme_minimal()
